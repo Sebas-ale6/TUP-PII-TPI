3a269d81e39c4d82d946824b4c7ce6f94ea5b632
OscarEMaciel <emaciel19@gmail.com>
2024-06-24 14:54:30 -0300

    1

--- TP python/jugador.py
+++ TP python/jugador.py
@@ -8,5 +8,4 @@ class Jugador:
         if tipo in self.habilidades:
             self.habilidades[tipo] = min(self.habilidades[tipo] + valor, 100)
             print(f"Ganaste {valor} puntos de experiencia en {tipo}. Nivel de {tipo} actual: {self.habilidades[tipo]}")
-        else:
-            raise ValueError(f"Tipo de experiencia desconocido: {tipo}")
+        
